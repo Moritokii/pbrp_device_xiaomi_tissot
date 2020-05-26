98d4219ef772c1e99042964e0b68c3d7652e51b1
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-05-26 11:39:00 +0800

    Fixed Everything and Rebased
    Added CircleCI Scripts
    
    fixed link
    
    Yet another link fix
    
    Fixed Out directory

---
+++ .circleci/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+repo init -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-9.0
+repo sync --force-sync
+git clone https://github.com/Takeshiro04/pbrp_device_xiaomi_tissot device/xiaomi/tissot/
+rm -rf hardware/qcom/bootctrl
+. build/envsetup.sh
+ALLOW_MISSING_DEPENDENCIES=true
+lunch omni_tissot-userdebug
+mka recoveryimage
+  ZIP=$(ls out/target/product/tissot/PitchBlack*.zip)
+curl https://bashupload.com/$ZIP --data-binary @$ZIP
