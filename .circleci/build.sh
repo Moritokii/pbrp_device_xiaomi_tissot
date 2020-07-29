1c835b6055bc714bad6debe0db89b2f748014624
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-07-29 16:47:57 +0800

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Takeshiro04/pbrp_device_xiaomi_tissot device/xiaomi
 rm -rf hardware/qcom/bootctrl
 . build/envsetup.sh
 ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_tissot-userdebug
+lunch omni_tissot-eng
 mka recoveryimage
   ZIP=$(ls out/target/product/tissot/PBRP*.zip)
 curl https://bashupload.com/$ZIP --data-binary @$ZIP
