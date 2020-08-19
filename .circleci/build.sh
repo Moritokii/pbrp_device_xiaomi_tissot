259f8e0402484ba70407c8c698e3885208b182bd
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-08-20 00:34:16 +0800

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,7 +2,7 @@
 repo init -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-9.0
 repo sync --force-sync
 git clone https://github.com/Takeshiro04/pbrp_device_xiaomi_tissot device/xiaomi/tissot/
-rm -rf hardware/qcom/bootctrl
+rm -rf device/xiaomi/tissot/bootctrl
 . build/envsetup.sh
 ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_tissot-eng
