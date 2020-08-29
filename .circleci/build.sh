7a03a0d18e56c6f1dbecd8890537985b9672e1c5
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-08-30 04:20:17 +0800

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,7 +2,7 @@
 repo init -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-9.0
 repo sync --force-sync
 git clone https://github.com/Takeshiro04/pbrp_device_xiaomi_tissot device/xiaomi/tissot/
-rm -rf device/xiaomi/tissot/bootctrl
+rm -rf hardware/qcom/bootctrl
 . build/envsetup.sh
 lunch omni_tissot-eng
 mka recoveryimage
